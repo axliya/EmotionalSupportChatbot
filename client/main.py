3beaf653e7b05d77e70f37a04fe48181cbc45c3e
axliya <60436117+axliya@users.noreply.github.com>
2022-03-28 21:10:24 -0400

    integrated application completed

--- client/main.py
+++ client/main.py
@@ -2,14 +2,37 @@ from flask import Flask, render_template,request,redirect,url_for
 from pymongo import MongoClient   
 from flask_pymongo import PyMongo 
 import os  
+from flask_socketio import SocketIO, emit
+from chatbotmain import chat
 
 app = Flask(__name__, template_folder='templates')
 client = MongoClient("mongodb+srv://test:test@cluster0.qlmo0.mongodb.net/EmotionalSupportChatbot?retryWrites=true&w=majority")
 db = client.EmotionalSupportChatbot
+app.config['SECRET_KEY'] = 'jsshiuehnksapasmxjsd'
+
+#initialize flask- SocketIO
+socketio = SocketIO(app)
 
 @app.route('/')
 def home():
-  return render_template('index.html')
+  return render_template('chatbox.html')
+
+def messageRecived():
+  print( 'message received !' )
+
+#send data to the server side events
+@socketio.on( 'my eventes' )
+def handleEvent( json1 ):
+  message = json1['message']
+
+  #print  message to  terminal
+  print(message)
+  answer=chat(message)
+  json1['answer'] = answer
+  json1['bot']='AI'
+  print( 'recived my event: ' + str(json1 ))
+  socketio.emit( 'my response', json1, callback=messageRecived )
+
 
 @app.route('/addtestimonial', methods=['GET', 'POST'])
 def addtestimonial():
@@ -46,4 +69,12 @@ def viewresources():
 
   return render_template("resources.html", resources=resources)
 
-app.run(host='0.0.0.0', port=8080)
+
+@app.route('/about', methods=['GET'])
+def renderabout():
+  return render_template("about.html")
+
+if __name__ == '__main__':
+  socketio.run( app )
+
+app.run(host='127.0.0.1', port=8080)
